ef8fcbb1435bb1451785776e5024813fca5def6e
Olonisakin David <olonisakin.david@stu.cu.edu.ng>
2023-09-04 13:27:20 +0100

    continuation to completion

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -17,7 +17,7 @@ class StartScreen extends StatelessWidget {
             height: 80,
           ),
           const Text(
-            'Learn Flutter the fun way!',
+            'Learn Flutter the fun way',
             style: TextStyle(
               color: Color.fromARGB(255, 255, 255, 255),
               fontSize: 24,
